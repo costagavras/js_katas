377fb9267d445b6dca7507694b3bc138e60a0c30
costagavras <cgavras@yahoo.ca>
2019-05-18 23:52:47 -0400

    kata6

--- kata6.js
+++ kata6.js
@@ -9,3 +9,29 @@ function addBinary(a,b) {
 
 
 addBinary(2,3);
+
+const addBinary = (a, b) => (a + b).toString(2);
+
+function addBinary(a,b){
+  return ((a + b) >>> 0).toString(2);
+}
+
+function addBinary(a,b) {
+  var c = a + b;
+  var res = '';
+  while (c >= 1) {
+    var res = c % 2 + res;
+    c = Math.floor(c / 2);
+  }
+  return res;
+}
+
+function addBinary(a,b) {
+  let num = a + b;
+  let bin = "";
+  while (num) {
+    bin = (num & 1) + bin;
+    num >>= 1;
+  }
+  return bin;
+}
